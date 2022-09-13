ae295a53ae6c5eb93b7c6229c830f2d161babd53
ViniciusFerreira55 <vinijoseferreira38@gmail.com>
2022-09-13 14:46:01 -0300

    Added tailwindcss to project using tailwind to style components

--- src/App.js
+++
@@ -1,7 +0,0 @@
-export const App = () => {
-  return(
-    <div>
-      <h1>salve tesao</h1>
-    </div>
-  )
-}
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import { Navbar } from './components/navbar/Navbar'
+
+export const App = () => {
+  return(
+    <Navbar></Navbar>
+  )
+}
