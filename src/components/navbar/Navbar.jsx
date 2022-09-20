b56e9600b4f2742b2bd747cb0ae8daed311c092d
ViniciusFerreira55 <vinijoseferreira38@gmail.com>
2022-09-20 09:51:01 -0300

    progresso navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,16 @@
+import Logo from "../../assets/logo.png"
+
 export const Navbar = () => {
     return (
-        <div>
-            
+        <div className="bg-purple-500 content-center rounded-b-lg flex justify-center">
+            <div className="object-scale-down h-48 w-96"><img src={Logo} alt="logo"/></div>
+            <ul> 
+                <li className="inline-block p-8"><a href="Vantagens">Vantagens</a></li>
+                <li className="inline-block p-8"><a href="Sobre">Sobre</a></li>
+                <li className="inline-block p-8"><a href="Cartões">Cartões</a></li>
+                <li className="inline-block p-8 rounded-full py-3 px-6 bg-purple-400"><a href="Login">Login</a></li>
+                <li className="inline-block p-8 rounded-full py-3 px-6 bg-purple-400"><a href="Cadastrar-se">Cadastrar-se</a></li>
+            </ul>
         </div>
     )
 }
