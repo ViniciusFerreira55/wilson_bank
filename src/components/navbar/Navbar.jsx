6309e53de44e962430ac4e0502f71914464838b1
ViniciusFerreira55 <vinijoseferreira38@gmail.com>
2022-09-20 10:26:33 -0300

    navbar no dale (quase)

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,14 +2,14 @@ import Logo from "../../assets/logo.png"
 
 export const Navbar = () => {
     return (
-        <div className="bg-purple-500 content-center rounded-b-lg flex justify-center">
-            <div className="object-scale-down h-48 w-96"><img src={Logo} alt="logo"/></div>
+        <div className="place-content-evenly bg-purple-500 content-center rounded-b-lg flex justify-center">
+            <div className="object-scale-down h-20 w-96"><img className="h-auto w-screen" src={Logo} alt="logo"/></div>
             <ul> 
                 <li className="inline-block p-8"><a href="Vantagens">Vantagens</a></li>
                 <li className="inline-block p-8"><a href="Sobre">Sobre</a></li>
                 <li className="inline-block p-8"><a href="Cartões">Cartões</a></li>
-                <li className="inline-block p-8 rounded-full py-3 px-6 bg-purple-400"><a href="Login">Login</a></li>
-                <li className="inline-block p-8 rounded-full py-3 px-6 bg-purple-400"><a href="Cadastrar-se">Cadastrar-se</a></li>
+                <li className="inline-block p-12 rounded-full py-3 px-6 bg-purple-400"><a href="Login">Login</a></li>
+                <li className="inline-block p-12 rounded-full py-3 px-6 bg-purple-400 last:ml-5"><a href="Cadastrar-se">Cadastrar-se</a></li>
             </ul>
         </div>
     )
