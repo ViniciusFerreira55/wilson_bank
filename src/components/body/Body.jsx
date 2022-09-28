b15cd9442d415aac593076f89aeceed3c62c2271
ViniciusFerreira55 <vinijoseferreira38@gmail.com>
2022-09-28 10:40:51 -0300

    lucid chart help me to make the first part of the body

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -3,10 +3,15 @@
 export const Body = () => {
     return (
         <div>
+            <div>
+            <div className="japonesa">
+                <h3>Wilsonstreet</h3>
+            </div>
             <div className="flex flex-row-reverse mr-80 mt-28 text-5xl"><h1>Venha fazer parte dessa caminhada</h1></div>
             <div className="flex flex-row-reverse mr-80 mt-20 text-5xl"><h2>Com WilsonStreet você </h2></div>
             <div className="flex flex-row-reverse mr-80 text-5xl"><h2>   caminha para o sucesso!</h2></div>
             <div className="flex flex-row-reverse mr-80 mt-28 text-5xl"><a href="Cadastrar-se" className="inline-block p-12 rounded-full py-3 px-6 bg-purple-400 last:ml-5">Cadastrar-se</a></div>
+        </div>
         </div>
     )
 }
