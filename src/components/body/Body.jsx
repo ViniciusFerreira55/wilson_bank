0e03f05c7b486d2da27ea74acf1ca4a664968eed
ViniciusFerreira55 <vinijoseferreira38@gmail.com>
2022-09-28 09:34:23 -0300

    i try to remake the body, some day i get good in css

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -1,22 +1,12 @@
-import Bg from "../../assets/bg.png"
-import JapaneaseGirl from "../../assets/japaneaseGirl.png"
+
 
 export const Body = () => {
     return (
         <div>
-            <div><img src={Bg} alt="background" />
-            <div >
-            <div className="content-center">
-                <img className="object-scale-down h-20 w-96" src={JapaneaseGirl} alt="japan girl" />
-            </div>
-            <div className="inline-block">
-                <h1>Venha fazer parte da nossa caminhada</h1>
-                <h2>Com WilsonStreet você caminha para o sucesso!</h2>
-                <a href="Cadastar" className="rounded-full py-3 px-6 bg-purple-400">Cadastrar</a>
-            </div>
-            </div>
-
-            </div>
+            <div className="flex flex-row-reverse mr-80 mt-28 text-5xl"><h1>Venha fazer parte dessa caminhada</h1></div>
+            <div className="flex flex-row-reverse mr-80 mt-20 text-5xl"><h2>Com WilsonStreet você </h2></div>
+            <div className="flex flex-row-reverse mr-80 text-5xl"><h2>   caminha para o sucesso!</h2></div>
+            <div className="flex flex-row-reverse mr-80 mt-28 text-5xl"><a href="Cadastrar-se" className="inline-block p-12 rounded-full py-3 px-6 bg-purple-400 last:ml-5">Cadastrar-se</a></div>
         </div>
     )
 }
