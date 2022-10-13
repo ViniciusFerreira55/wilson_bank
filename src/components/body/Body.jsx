b0d7d695db363993283a0008f6847d6df7ba96a5
ViniciusFerreira55 <vinijoseferreira38@gmail.com>
2022-10-13 16:46:14 -0300

    just add somethings in the body

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -1,16 +1,23 @@
-
+import Card from '../../assets/card.png'
 
 export const Body = () => {
     return (
         <div>
             <div>
-            <div className="japonesa">
-                <h3>Wilsonstreet</h3>
+            <div className="flex flex-col japonesa bg-[url('../src/assets/japaneaseGirl.png')] bg-no-repeat h-[40rem] bg-cover  p-40  text-right text-2xl mt-8">
+                <h1>Wilsonstreet</h1>
+                <h2>Venha fazer parte dessa caminhada</h2>
+                <h3>Com WilsonStreet você </h3>
+                <h3>   caminha para o sucesso!</h3>
+                <a href="Cadastrar-se" className="inline-block p-12 rounded-full py-3 px-6 bg-purple-400 last:ml-5 w-[12rem] self-end mt-4">Cadastrar-se</a>
+
             </div>
-            <div className="flex flex-row-reverse mr-80 mt-28 text-5xl"><h1>Venha fazer parte dessa caminhada</h1></div>
-            <div className="flex flex-row-reverse mr-80 mt-20 text-5xl"><h2>Com WilsonStreet você </h2></div>
-            <div className="flex flex-row-reverse mr-80 text-5xl"><h2>   caminha para o sucesso!</h2></div>
-            <div className="flex flex-row-reverse mr-80 mt-28 text-5xl"><a href="Cadastrar-se" className="inline-block p-12 rounded-full py-3 px-6 bg-purple-400 last:ml-5">Cadastrar-se</a></div>
+           
+        </div>
+        <div>
+            <div><h1>Receba cashback e milhas sem anuidade!!</h1></div>
+            <div><img src={Card} alt="card" /></div>
+            
         </div>
         </div>
     )
