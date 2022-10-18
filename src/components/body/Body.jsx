2ff10310ec8079db30e5d69d964f81042742ce4d
ViniciusFerreira55 <vinijoseferreira38@gmail.com>
2022-10-18 10:29:01 -0300

    i almost finish the body, but i will probably just made some more css, now i will start the footer

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -1,10 +1,14 @@
 import Card from '../../assets/card.png'
+import IconCard from '../../assets/cartao.png'
+import IconPix from '../../assets/pix.png'
+import IconInvestimento from '../../assets/investimento.png'
+import IconTransferencia from '../../assets/transferencia.png'
 
 export const Body = () => {
     return (
         <div>
             <div>
-            <div className="flex flex-col japonesa bg-[url('../src/assets/japaneaseGirl.png')] bg-no-repeat h-[40rem] bg-cover  p-40  text-right text-2xl mt-8">
+            <div className="flex flex-col japonesa bg-[url('../src/assets/japaneaseGirl.png')] bg-no-repeat h-[40rem] bg-cover  p-40  text-right text-2xl">
                 <h1>Wilsonstreet</h1>
                 <h2>Venha fazer parte dessa caminhada</h2>
                 <h3>Com WilsonStreet você </h3>
@@ -14,10 +18,23 @@ export const Body = () => {
             </div>
            
         </div>
-        <div>
-            <div><h1>Receba cashback e milhas sem anuidade!!</h1></div>
-            <div><img src={Card} alt="card" /></div>
+        <div className='conheca'>
+            <div>
+            <div className='text-2xl pl-8 pt-8 flex'><h1>Conheça o cartão com os melhores benefícios</h1></div>
+            <div className='text-xl pl-8 pt-8'><h1>Receba cashback e milhas sem anuidade!!</h1></div>
+            <a href="Cadastrar-se" className="inline-block p-12 rounded-full py-3 px-6 bg-purple-400 last:ml-5  self-end mt-4">Saiba mais</a>
+            </div>
             
+            <div className='float-right pr-60'><img src={Card} alt="card" className='h-44 w-64'/></div>
+        </div>
+        <div className='pt-10'>
+            <div className='text-2xl mb-20 text-center'><h1>Nossos Serviços Mais Utilizados</h1></div>
+                <div className='img'>
+                    <a href="a"><img src={IconCard} alt="cartao"/></a>
+                    <a href="a"><img src={IconPix} alt="pix"/></a>
+                    <a href="a"><img src={IconInvestimento} alt="investimento"/></a>
+                    <a href="a"><img src={IconTransferencia} alt="transferencia"/></a>
+                </div>
         </div>
         </div>
     )
