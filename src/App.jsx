8c26d271ebd10c0e0b0574563eb52a35628a83ff
ViniciusFerreira55 <vinijoseferreira38@gmail.com>
2022-10-18 11:25:23 -0300

    Just mixed the other 3 components in other, just to make it easy the logic of routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import { Navbar } from './components/navbar/Navbar'
-import { Body } from './components/body/Body'
-import { Footer } from './components/footer/Footer'
-
+import {Home} from './components/HomePage/Home'
 
 export const App = () => {
   return(
     <div>
-    <Navbar />
-    <Body />
-    <Footer/>
+    <Home/>
     </div>
   )
 }
---
+++ src/components/HomePage/Home.jsx
@@ -0,0 +1,14 @@
+import { Navbar } from '../navbar/Navbar'
+import { Body } from '../body/Body'
+import { Footer } from '../footer/Footer'
+
+
+export const Home = () => {
+  return(
+    <div>
+    <Navbar />
+    <Body />
+    <Footer/>
+    </div>
+  )
+}
