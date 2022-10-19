10900acfa34151ed2753925299c510ea90ec6c7f
ViniciusFerreira55 <vinijoseferreira38@gmail.com>
2022-10-19 10:01:06 -0300

    card page almost ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 // import {Home} from './components/HomePage/Home'
 // import { TransferencePage } from './components/TransferencePage/TransferencePage'
 // import { InvestimentPage } from './components/InvestimentPage/InvestimentePage'
-import { PixPage } from './components/PixPage/PixPage'
-
+// import { PixPage } from './components/PixPage/PixPage'
+import { CardPage } from "./components/CardsPage/CardPage"
 export const App = () => {
   return(
     <div>
-    <PixPage/>
+    <CardPage/>
     </div>
   )
 }
