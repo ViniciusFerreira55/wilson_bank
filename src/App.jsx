313ee48ba13aad455a08a792c3f50c8506086200
ViniciusFerreira55 <vinijoseferreira38@gmail.com>
2022-10-19 09:32:08 -0300

    another info page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 // import {Home} from './components/HomePage/Home'
 // import { TransferencePage } from './components/TransferencePage/TransferencePage'
-import { InvestimentPage } from './components/InvestimentPage/InvestimentePage'
+// import { InvestimentPage } from './components/InvestimentPage/InvestimentePage'
+import { PixPage } from './components/PixPage/PixPage'
+
 export const App = () => {
   return(
     <div>
-    <InvestimentPage/>
+    <PixPage/>
     </div>
   )
 }
---
+++ src/components/PixPage/PixPage.jsx
@@ -0,0 +1,35 @@
+import { Navbar } from "../navbar/Navbar"
+import { Footer } from "../footer/Footer"
+import PixGirl from "../../assets/PixGirl.png"
+import PixIcon from "../../assets/pix.png"
+
+
+export const PixPage = () =>{
+    return(
+        <div>
+            <Navbar/>
+            <div className="pt-8 flex">
+            <div>
+                <img src={PixIcon} alt="Pix icon"  className="h-44 w-44 absolute ml-72"/>
+                </div>
+            <div className="mt-64 ml-60 text-2xl">
+                    <h2>Lorem ipsum dolor sit</h2>
+                    <h2>amet, consectetur</h2>
+                    <h2>adipiscing elit, sed do</h2>
+                    <h2>eiusmod tempor</h2>
+                    <h2>incididunt ut labore et</h2>
+                    <h2>dolore magna aliqua.</h2>
+                    <h2>Ut enim ad minim</h2>
+                    <h2>veniam, quis nostrud</h2>
+                    <h2>exercitation ullamco</h2>
+                    <h2>laboris nisi ut aliquip</h2>
+                </div>
+                
+                <img src={PixGirl} alt="PixGirl" className="PixGirl"/>
+            </div>
+            <div>
+                <Footer/>
+            </div>
+        </div>
+    )
+}
