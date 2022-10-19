47328d21b94411698ce2d4f2ed0733e1152c56e0
ViniciusFerreira55 <vinijoseferreira38@gmail.com>
2022-10-19 08:55:21 -0300

    OHHHH yeah, another info page finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 // import {Home} from './components/HomePage/Home'
-import { TransferencePage } from './components/TransferencePage/TransferencePage'
+// import { TransferencePage } from './components/TransferencePage/TransferencePage'
+import { InvestimentPage } from './components/InvestimentPage/InvestimentePage'
 export const App = () => {
   return(
     <div>
-    <TransferencePage/>
+    <InvestimentPage/>
     </div>
   )
 }
---
+++ src/components/InvestimentPage/InvestimentePage.jsx
@@ -0,0 +1,32 @@
+import { Navbar } from "../navbar/Navbar";
+import { Footer } from "../footer/Footer";
+import Man2 from "../../assets/investimentMan.png"
+import InvestimentIcon from "../../assets/investimento.png"
+export const InvestimentPage = () =>{
+    return(
+        <div>
+            <Navbar/>
+            <div className="pt-8 flex">
+                <img src={Man2} alt="Man" className="manInvestiment"/>
+                <div>
+                    <img src={InvestimentIcon} alt="transference icon"  className="h-44 w-44 right-0 absolute mr-64"/>
+                </div>
+                <div className="mt-64 ml-80 text-2xl">
+                    <h2>Lorem ipsum dolor sit</h2>
+                    <h2>amet, consectetur</h2>
+                    <h2>adipiscing elit, sed do</h2>
+                    <h2>eiusmod tempor</h2>
+                    <h2>incididunt ut labore et</h2>
+                    <h2>dolore magna aliqua.</h2>
+                    <h2>Ut enim ad minim</h2>
+                    <h2>veniam, quis nostrud</h2>
+                    <h2>exercitation ullamco</h2>
+                    <h2>laboris nisi ut aliquip</h2>
+                </div>
+            </div>
+            <div>
+            <Footer/>
+            </div>
+        </div>
+    )
+}
