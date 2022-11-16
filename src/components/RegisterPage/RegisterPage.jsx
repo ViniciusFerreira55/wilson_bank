a69128affd2881052cd90b86b2d47d3b43395452
ViniciusFerreira55 <vinijoseferreira38@gmail.com>
2022-11-16 11:20:24 -0300

    cadastrando na api

--- src/components/RegisterPage/RegisterPage.jsx
+++ src/components/RegisterPage/RegisterPage.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { Navbar } from "../navbar/Navbar";
 
 export const Register = () => {
@@ -9,12 +10,18 @@ export const Register = () => {
     const [email, setEmail] = useState('')
     const [senha, setSenha] = useState('')
     const [confirmarSenha, setConfirmarSenha] = useState('')
-    const [foto, setFoto] = useState('')
+    const navigate = useNavigate();
+    // const [foto, setFoto] = useState('')
+    // const [data, setData] = useState({
+    //     title: "",
+    //     description: "",
+    //     image_url: "",
+    // });
 
     const validarCadastro = () => {
         console.log(senha)
         console.log(confirmarSenha)
-        if (senha != confirmarSenha){
+        if (senha !== confirmarSenha){
             return console.log('zimbas')
         }
 
@@ -22,16 +29,40 @@ export const Register = () => {
         
     } 
 
+    
+    // const handleImageChange = (e) => {
+    //     let newData = { ...data };
+    //     newData["image_url"] = e.target.files[0];
+    //     setData(newData);
+    //     setFoto( e.target.files[0])
+    //     console.log(newData);
+    // };
+ 
     const cadastrar = () => {
 
         console.log("Enviar pra api...")
-        const objCliente = {nome: nome, cfp: cfp, dataNascimento: dataNascimento, email: email, senha: senha, foto: foto}
+
+        // let form_data = new FormData();
+        // form_data.append("foto", foto);
+        // form_data.append("nome", nome);
+        // form_data.append("cpf", cfp);
+        // form_data.append("data_nascimento", dataNascimento);
+        // form_data.append("email", email);
+        // form_data.append("password", senha);
+        // form_data.append("foto", foto);
+        // console.log(form_data)
+
+
+        const objCliente = {nome: nome, cpf: cfp, data_nascimento: dataNascimento, email: email, password: senha}
+        console.log(objCliente)
         const headers = {
             'Content-Type': 'application/json',
           };
-        fetch(urlApi + '/clientes/', { method: 'POST', headers, body: objCliente}).then(res => res.json()).then(dd =>{ 
+        fetch(urlApi + '/clientes/', { method: 'POST', headers, body: JSON.stringify(objCliente)}).then(res => res.json()).then(dd =>{ 
             console.log(dd)
         })
+
+        navigate('/')
     }
 
     return(
@@ -40,16 +71,23 @@ export const Register = () => {
             <Navbar/>
             <div className="fundo2">
             <div className="ml-36 inline-grid mt-32 ml-96">
-                    <ul className="formulario ">
-                    <li><input type="text" onChange={e => setNome(e.target.value)} name="nome" placeholder="Nome" className="border-4 mb-4 p-5 border-indigo-600 h-14 w-96"/></li>
-                    <li><input type="text" onChange={e => setCpf(e.target.value)} name="cpf" placeholder="CPF" className="border-4 mb-4 p-5 border-indigo-600 h-14 w-96"/></li>
+                    <form className="formulario list-none">
+                    <h1>Nome</h1>
+                    <li><input type="text" onChange={e => setNome(e.target.value)} name="nome" placeholder="Wilson" className="border-4 mb-4 p-5 border-indigo-600 h-14 w-96"/></li>
+                    <h1>CPF</h1>
+                    <li><input type="text" onChange={e => setCpf(e.target.value)} name="cpf" placeholder="12345678912" className="border-4 mb-4 p-5 border-indigo-600 h-14 w-96"/></li>
+                    <h1>Data de Nascimento</h1>
                     <li><input type="date" onChange={e => setDataNascimento(e.target.value)} name="dataNas" placeholder="Data de nascimento" className="border-4 mb-4 p-5 border-indigo-600 h-14 w-96"/></li>
-                    <li><input type="email" onChange={e => setEmail(e.target.value)} name="email"  placeholder="Email" className="border-4 mb-4 p-5 border-indigo-600 h-14 w-96"/></li>
-                    <li><input type="password" onChange={e => setSenha(e.target.value)} name="senha" placeholder="senha" className="border-4 mb-4 p-5 border-indigo-600 h-14 w-96"/></li>
-                    <li><input type="password"  onChange={e => setConfirmarSenha(e.target.value)}  name="confirmar" placeholder="Confirmar Senha" className="border-4 mb-4 p-5 border-indigo-600 h-14 w-96"/></li>
-                    <li><input type="file" onChange={e => setFoto(e.target.value)} name="photo" placeholder="Photo" className="border-4 mb-4 p-5 border-indigo-600 h-14 w-96"/></li>
-                    <li className="text-center"><a className="inline-block p-12 rounded-full py-3 px-6 bg-purple-400 last:ml-5"><button onClick={validarCadastro}>Cadastrar-se</button></a></li>
-                    </ul>
+                    <h1>Email</h1>
+                    <li><input type="email" onChange={e => setEmail(e.target.value)} name="email"  placeholder="jonas@gmail.com" className="border-4 mb-4 p-5 border-indigo-600 h-14 w-96"/></li>
+                    <h1>Senha</h1>
+                    <li><input type="password" onChange={e => setSenha(e.target.value)} name="senha" placeholder="123456" className="border-4 mb-4 p-5 border-indigo-600 h-14 w-96"/></li>
+                    <h1>Confirmar Senha</h1>
+                    <li><input type="password"  onChange={e => setConfirmarSenha(e.target.value)}  name="confirmar" placeholder="123456" className="border-4 mb-4 p-5 border-indigo-600 h-14 w-96"/></li>
+                    {/* <h1>Foto</h1>
+                    <li><input type="file" onChange={(e) => {handleImageChange(e)}}  name="photo" placeholder="Photo" className="border-4 mb-4 p-5 border-indigo-600 h-14 w-96"/></li> */}
+                    <li className="text-center"><a className="inline-block p-12 rounded-full py-3 px-6 bg-purple-400 last:ml-5"><button type="button" onClick={validarCadastro}>Cadastrar-se</button></a></li>
+                    </form>
             </div>
         </div>
         </div>
