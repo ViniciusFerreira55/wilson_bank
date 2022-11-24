321f4c1fa7d2336de4c0dd1d3d1bfc24c5d15f3b
ViniciusFerreira55 <vinijoseferreira38@gmail.com>
2022-11-24 11:22:52 -0300

    Card is showing, just need to put the info on it

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -8,7 +8,9 @@ export const Card = () => {
     if (session.conta.cardImage === "3") {
         return (
             <div className="bg-[url('../src/assets/luffyCard.png')] bg-no-repeat h-[23rem] w-[40rem] bg-center">
-                <div className="text-orange-400 text-2xl">WilsonStreet</div>
+                <div className="text-2xl">WilsonStreet</div>
+                <div className="text-2xl m-40">{session.conta.numeroCartao}</div>
+                <div className="text-2xl">{session.conta.validade}</div>
             </div>
         )
 
